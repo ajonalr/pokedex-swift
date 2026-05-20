f07789a163f79d687b34e78529ece38e8e4b750f
Ariel Ramirez <ariel12jona@gmail.com>
2026-05-20 11:05:38 -0600

    Initial Commit

---
+++ Pockedex/PockedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockedexApp.swift
+//  Pockedex
+//
+//  Created by Ariel Ramirez on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PockedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
